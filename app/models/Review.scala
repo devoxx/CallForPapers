c01bb16890fc1a1a129e821f5da9eb81ca226883
Stephan Janssen <sja@bejug.org>
2016-10-13 15:04:14 +0200

    LeaderBaord cleanup for totalCommentsForProposals

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -250,7 +250,7 @@ object Review {
   def totalInternalCommentsPerProposal(): List[(String, Int)] = {
     val proposalsCommentsCount: List[(String, Int)] = {
       allProposalsAndReviews.map(proposal => (proposal._1, Comment.allInternalComments(proposal._1).size))
-                              .filter(_._2 > 1) // At least two comments
+                              .filter(_._2 > 2) // At least three comments
     }
 
     proposalsCommentsCount
