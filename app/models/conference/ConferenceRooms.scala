24f5fc08c159368e87489a4ce5ed812759b792f9
Stephan Janssen <sja@bejug.org>
2016-11-30 14:23:27 +0100

    Moved Ignite talks to room 4

--- app/models/conference/ConferenceRooms.scala
+++ app/models/conference/ConferenceRooms.scala
@@ -59,7 +59,7 @@ object ConferenceRooms {
 
   val allRoomsBOF = List(ROOM5, ROOM6, ROOM7, ROOM8, ROOM9, ROOM10)
 
-  val igniteRoom = List(ROOM7)
+  val igniteRoom = List(ROOM4)
 
   val allRooms = List(ROOM1, ROOM2, ROOM3, ROOM4, ROOM5, ROOM6, ROOM7, ROOM8, ROOM9, ROOM10)
 }
