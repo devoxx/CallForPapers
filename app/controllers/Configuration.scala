e6331f5263c3f3cfb50f102a027345e33dda5759
Stephan Janssen <sja@bejug.org>
2017-08-03 15:31:13 +0200

    Draft import config

--- app/controllers/Configuration.scala
+++ app/controllers/Configuration.scala
@@ -1,15 +1,35 @@
 package controllers
 
 import models.ConferenceDescriptor
+import play.api.i18n.Messages
 import play.api.mvc.{Action, AnyContent}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints.nonEmpty
 
 /**
   * @author Stephan Janssen
   */
 object Configuration extends SecureCFPController {
 
+  val configForm = Form("json" -> (text verifying nonEmpty))
+
   def showConfig: Action[AnyContent] = SecuredAction(IsMemberOf("admin")) {
     implicit request: SecuredRequest[play.api.mvc.AnyContent] =>
       Ok(views.html.Configuration.showConfiguration(ConferenceDescriptor.current()))
+  }
+
+  def showImportConfig: Action[AnyContent] = SecuredAction(IsMemberOf("admin")) {
+    implicit request: SecuredRequest[play.api.mvc.AnyContent] =>
+      Ok(views.html.Configuration.importConfiguration(configForm))
+  }
+
+  def doImportConfig() = Action {
+    implicit request =>
+      configForm.bindFromRequest.fold(
+        invalidForm => BadRequest(views.html.Configuration.importConfiguration(invalidForm)).flashing("error" -> Messages("Something went wrong, check logs!")),
+        validForm => Redirect(routes.Configuration.showConfig()).flashing("success" -> Messages("Configuration updated!"))
+
+      )
   }
 }
