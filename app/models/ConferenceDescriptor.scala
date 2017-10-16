8eeb96a2aa5513a9d9a868d09317efa1befb67af
Stephan Janssen <sja@bejug.org>
2017-10-16 16:56:11 +0200

    Moved Wed. lab slot to 14h00 - 17h00

--- app/models/ConferenceDescriptor.scala
+++ app/models/ConferenceDescriptor.scala
@@ -636,8 +636,8 @@ object ConferenceDescriptor {
         r1 =>
           SlotBuilder(ConferenceProposalTypes.LAB.id,
             WEDNESDAY,
-            new DateTime(TUE_DATE + "13:30" + MIN_SEC).toDateTime(DateTimeZone.forID(europeBrussels)),
-            new DateTime(TUE_DATE + "16:30" + MIN_SEC).toDateTime(DateTimeZone.forID(europeBrussels)), r1)
+            new DateTime(TUE_DATE + "14:00" + MIN_SEC).toDateTime(DateTimeZone.forID(europeBrussels)),
+            new DateTime(TUE_DATE + "17:00" + MIN_SEC).toDateTime(DateTimeZone.forID(europeBrussels)), r1)
       }
 
       labsTuesdayMorning ++ 
